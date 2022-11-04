bddb40c398f7676d855aa4acc4e7979781907378
josephwwilliams <josephwwilliams18@gmail.com>
2022-11-03 23:12:25 -0500

    Update material.module.ts

--- healthy-u/src/app/shared/material.module.ts
+++ healthy-u/src/app/shared/material.module.ts
@@ -41,7 +41,6 @@ import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatTreeModule } from '@angular/material/tree';
-import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
 @NgModule({
   exports: [
     A11yModule,
@@ -88,7 +87,6 @@ import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
     PortalModule,
     ScrollingModule,
     MatExpansionModule,
-    MatBottomSheetModule,
   ],
   declarations: [],
 })
